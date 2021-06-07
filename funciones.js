7d176eec7d9c3303d6635d97bcd48e3a7fecba6a
Michael-JHL <81574296+Michael-JHL@users.noreply.github.com>
2021-06-07 09:06:08 -0500

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,12 @@
+function mostrarPato(){
+				var $Hola= "La tendencia en tus manos "
+				alert($Hola)
+}
+function resultadoClick($Mensaje){
+				var $Mensaje= "Bienvenido ahora eres parte de esta comunidad ";
+				alert($Mensaje);
+}
+function registroPagina(){
+				var $Registro= "Registrate y aprovecha de los beneficios esclusivos "
+				alert($Registro)
+}
